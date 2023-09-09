f87014c2413ca28861690a0b131ca33931cef918
Alexandre Bezroutchko <abb@gremwell.com>
2023-09-09 00:36:35 +0000

    bugfix

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -xe
 
-apk add python3 py3-pip gcc python3-dev musl-dev linux-headers tmux npm bash
+apk add python3 py3-pip py3-virtualenv
 
-python3 -m pip install -U pip
-python3 -m pip install -U virtualenv
-
-python3 -m virtualenv venv
+python3 -m venv venv
 source venv/bin/activate
+python3 -m pip install -U pip
 
 pip install -r requirements.txt
